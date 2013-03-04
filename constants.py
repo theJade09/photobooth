f6873873736556329362e692afd004941d237172
Stanley Wong <wongsk04@yahoo.com>
2013-03-04 10:52:29 -0800

    Adding Jquery libraries. Modified constants to reflect actual camera usage.  Added non-functional cosmetics to index.html

--- constants.py
+++ constants.py
@@ -1,16 +1,21 @@
 import os
 
-BASE_DIR = '/Users/BunBe/Sites'
-PHOTOBOOTH_DIRECTORY = 'photobooth'
-DIRECTORY_TO_WATCH = os.path.join(BASE_DIR, PHOTOBOOTH_DIRECTORY)
-RESIZE_DIRECTORY = os.path.join(DIRECTORY_TO_WATCH, 'small')
+BASE_DIR = '/Users/BunBe/Sites/'
+HOME_FOLDER = 'photobooth'
+WATCH_FOLDER = 'CANON'
+DIRECTORY_TO_WATCH = os.path.join(BASE_DIR, HOME_FOLDER, WATCH_FOLDER)
+RESIZE_DIRECTORY = os.path.join(BASE_DIR, HOME_FOLDER, 'small')
 
 if not os.path.isdir(RESIZE_DIRECTORY):
  os.mkdir(RESIZE_DIRECTORY)
 
+if not os.path.isdir(DIRECTORY_TO_WATCH):
+ os.mkdir(DIRECTORY_TO_WATCH)
+
 HOSTNAME = 'macair.local'
 USERNAME = 'BunBe'
-PHOTOBOOTH_URL = 'http://%s/~%s/%s' % (HOSTNAME, USERNAME, PHOTOBOOTH_DIRECTORY)
+IMAGE_PATH = '%s/%s/%s' % (HOME_FOLDER, WATCH_FOLDER, 'small')
+PHOTOBOOTH_URL = 'http://%s/~%s/%s' % (HOSTNAME, USERNAME, IMAGE_PATH)
 
 WS_PATH = '/new_photo'
 WS_URL = 'ws://%s:8888/%s' % (HOSTNAME, WS_PATH)
